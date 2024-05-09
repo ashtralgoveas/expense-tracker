59e5f40527a0dc416cda24a8a0416ceb2d794412
Ashtral Lorien Goveas <ashtralgoveas@gmail.com>
2024-05-09 16:00:18 +0530

    added changes

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,11 @@ export default [
     rules: {
       "no-unused-vars": "error",
       "no-undef": "error",
+      "react/prop-types": "off",
+      "no-console": "error",
+      "react/require-default-props": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/react-in-jsx-scope": "off",
     },
   },
 ];
